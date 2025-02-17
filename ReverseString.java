d9ad86509ffa6256d9a79be76a6278b8dd1afc05
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-17 23:29:03 +0100

    DONE

--- ReverseString.java
+++ ReverseString.java
@@ -1,8 +1,19 @@
+import java.lang.StringBuilder;
+import java.io.*;
+import java.util.*;
+
 public class ReverseString
 {
 	public static void main(String[] args)
 	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Vnesi poljuben niz: ");
+		String a = in.next();
+		StringBuilder niz = new StringBuilder();
 
-		
+		for(int i=a.length(); i>0; i--){
+			niz.append(a.charAt(i-1));
+		}
+		System.out.println(niz);
 	}
 }
